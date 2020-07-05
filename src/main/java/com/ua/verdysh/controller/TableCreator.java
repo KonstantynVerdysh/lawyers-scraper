ff061726c2b8d95a474b9f31ea8df0587cdba9fb
Konstantyn Verdysh <verdysh1991@gmail.com>
2020-07-05 13:14:01 +0300

    Fix commit

--- src/main/java/com/ua/verdysh/controller/TableCreator.java
+++ src/main/java/com/ua/verdysh/controller/TableCreator.java
@@ -17,13 +17,11 @@ public class TableCreator {
     private static final String FILE_NAME = "lawyers.xls";
 
     public void createTable(Map<String, List<LawyerProfile>> profiles) {
-        try {
-            try (Workbook workbook = new HSSFWorkbook()) {
-                for (Map.Entry<String, List<LawyerProfile>> entry : profiles.entrySet()) {
-                    createSheet(workbook, entry.getKey(), entry.getValue());
-                }
-                writeToFile(workbook);
+        try (Workbook workbook = new HSSFWorkbook()) {
+            for (Map.Entry<String, List<LawyerProfile>> entry : profiles.entrySet()) {
+                createSheet(workbook, entry.getKey(), entry.getValue());
             }
+            writeToFile(workbook);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
@@ -53,48 +51,3 @@ public class TableCreator {
         }
     }
 }
-
-
-
-//public class TableCreator {
-//    private static final String FILE_NAME = "auto.xls";
-//    private static final String SHEET_NAME = "auto";
-//
-//    public void createTable(List<Advertisement> advertisements) {
-//        try (Workbook workbook = new HSSFWorkbook()) {
-//            Sheet sheet = workbook.createSheet(SHEET_NAME);
-//            createRow(advertisements, sheet);
-//            writeToFile(workbook);
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
-//        }
-//    }
-//
-//    private void createRow(List<Advertisement> advertisements, Sheet sheet) {
-//        for (int count = 0; count < advertisements.size(); count++) {
-//            Advertisement advertisement = advertisements.get(count);
-//            Row row = sheet.createRow(count);
-//            row.setHeight((short) -1);
-//
-//            List<String> advertisementFields = new ArrayList<>(Arrays.asList(advertisement.getUrl(), advertisement.getPriceUsd(),
-//                    advertisement.getPriceUah(), advertisement.getHeading(), advertisement.getCity(),
-//                    advertisement.getSeller(), advertisement.getPhoto().toString(), advertisement.getDescription(),
-//                    advertisement.getId()));
-//
-//            advertisementFields.forEach(v -> createCell(row, advertisementFields.indexOf(v), v));
-//        }
-//    }
-//
-//    private void createCell(Row row, int number, String text) {
-//        Cell cellUrl = row.createCell(number);
-//        cellUrl.setCellValue(text);
-//    }
-//
-//    private void writeToFile(Workbook workbook) {
-//        try (FileOutputStream fos = new FileOutputStream(FILE_NAME)) {
-//            workbook.write(fos);
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
-//        }
-//    }
-//}
