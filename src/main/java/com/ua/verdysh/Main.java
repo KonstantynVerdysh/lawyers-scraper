a5c3871ee124c8d8656186db60a86d34e7730e0b
Verdysh <verdysh1991@gmail.com>
2020-07-06 17:15:17 +0300

    Fix commit

--- src/main/java/com/ua/verdysh/Main.java
+++ src/main/java/com/ua/verdysh/Main.java
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "geckodriver");
         WebDriver driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
